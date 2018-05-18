1c71ec61fcd5e065433a2c6eefe65166bb433c01
Ravern Koh <ravernkoh@gmail.com>
2018-05-18 12:46:01 +0800

    Fix formatting in challenge 2

--- challenges/02-conditional-statements/solution.py
+++ challenges/02-conditional-statements/solution.py
@@ -2,22 +2,22 @@
 height_str = input("What is your height (cm)? ")
 weight_str = input("What is your weight (kg)? ")
 
-#Convert user input string to floats
+# Convert user input string to floats
 height = float(height_str)
 weight = float(weight_str)
 
-#Convert height in cm to height in meters
+# Convert height in cm to height in meters
 height = height / 100
 
-#Calculate BMI
+# Calculate BMI
 bmi = weight / (height * height)
 
-#Check the BMI status
+# Check the BMI status
 if bmi < 18.5:
     print("Your BMI is %.2f and you are underweight" % bmi)
-elif bmi >=18.5 and bmi <= 24.9:
+elif bmi >= 18.5 and bmi <= 24.9:
     print("Your BMI is %.2f and you are of normal weight :)" % bmi)
-elif bmi >=25.0 and bmi <= 29.9:
+elif bmi >= 25.0 and bmi <= 29.9:
     print("Your BMI is %.2f and you are overweight" % bmi)
-elif bmi > 30.0:
+else:
     print("Your BMI is %.2f and you are obese" % bmi)
