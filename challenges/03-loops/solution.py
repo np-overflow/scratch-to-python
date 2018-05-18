ac28a105b08c4bb3073fa2d3897d4aa9a3c84db6
Yadunand Prem <yadunut@gmail.com>
2018-05-18 18:56:23 +0800

    Update solution

--- challenges/03-loops/solution.py
+++ challenges/03-loops/solution.py
@@ -1,15 +1,11 @@
 # Ask for width of the pyramid
-width_str = input("What is the radius of the base of the pyramid? ")
+radius_str = input("What is the radius of the base of the pyramid? ")
 
 # Convert the width from string to int
-width = int(radius_str)
-
-# hWidth is half the width of the pyramid
-hWidth = width // 2 
-
+radius = int(radius_str)
 
 # Draw the pyramid
-for i in range(1, hWidth+1):
+for i in range(1, radius+1):
     message = " " * (radius -i)
     message += "*" * (i*2)
     message += " " * (radius -i)
