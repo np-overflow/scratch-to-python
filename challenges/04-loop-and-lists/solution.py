b403f9f2aa49c34e08a2b0dee091d0350214f90c
Yadunand Prem <yadunut@gmail.com>
2018-05-21 21:22:38 +0800

    Update solution for challenge 4

--- challenges/04-loop-and-lists/solution.py
+++ challenges/04-loop-and-lists/solution.py
@@ -8,15 +8,6 @@ names = names_str.split(",")
 for index, name in enumerate(names):
     #Remove spaces from the start and end of the name
     name = name.strip()
-    message = "Hello " + name + ". You are the "
-    if (index + 1) % 10 == 1:
-        message += str(index+1) + "st"
-    elif (index + 1) % 10 == 2:
-        message += str(index+1) + "nd"
-    elif (index + 1) % 10 == 3:
-        message += str(index+1) + "rd"
-    else:
-        message += str(index+1) + "th"
-    message += " Person."
+    message = "Hello " + name + ". You are No. " + str(index + 1) + "in the list."
     print(message)
 
